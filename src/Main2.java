5c8113a503b75e21910dd8e2c0dad0c7406f6c10
sunghee-cho <bluejeansh@hanmail.net>
2015-07-20 13:37:18 +0900

    Signed-off-by: sunghee-cho <bluejeansh@hanmail.net>

---
+++ src/Main2.java
@@ -0,0 +1,12 @@
+
+public class Main2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("main2-workspace_project");
+	}
+
+}
