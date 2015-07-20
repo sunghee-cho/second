29f21470e8df7af393d5926a55491d23542e93ba
sunghee-cho <bluejeansh@hanmail.net>
2015-07-20 13:40:01 +0900

    Change-Id: I07a0c5ba64aa30a2f0f68d69bccb7b65b250d08d Signed-off-by: sunghee-cho <bluejeansh@hanmail.net>

---
+++ src/Main3.java
@@ -0,0 +1,12 @@
+
+public class Main3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("∏ﬁ¿Œ2-workspace_myspring");
+	}
+
+}
